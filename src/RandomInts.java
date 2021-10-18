4aebe8487f7ef333fca87030ca1b8382757b5b0b
puppyswan <92048050+puppyswan@users.noreply.github.com>
2021-10-18 13:12:43 -0500

    fixed RandomInts.java

--- src/RandomInts.java
+++ src/RandomInts.java
@@ -2,13 +2,14 @@ public class RandomInts {
 
     protected int[] numbers;
 
-    public RandomInts(){
+    public RandomInts() {
         numbers = new int[10];
+        for (int i = 0; i < 10; i++) {
+            numbers[i] = (int)(Math.random() * 10);
+        }
     }
 
     public int[] getNumbers() {
         return numbers;
     }
-
-
 }
