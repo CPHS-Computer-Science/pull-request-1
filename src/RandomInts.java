31790b92bc558cd449cb66f0c493c84c7d807a19
S-A-2019 <slava.andrianov66@k12.leanderisd.org>
2021-10-18 13:13:23 -0500

    fixed randomInts to generate random numbers from 1-10

--- src/RandomInts.java
+++ src/RandomInts.java
@@ -4,6 +4,10 @@ public class RandomInts {
 
     public RandomInts(){
         numbers = new int[10];
+        for(int i = 0; i < numbers.length; i++){
+            numbers[i] = (int)(Math.random() * 10)+1;
+        }
+
     }
 
     public int[] getNumbers() {
