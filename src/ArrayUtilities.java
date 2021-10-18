4e1329c305c6066d5884cdd50aced6e7be75cf82
Zach <zach.bauman12@gmail.com>
2021-10-18 13:07:37 -0500

    fixed arrayult

--- src/ArrayUtilities.java
+++ src/ArrayUtilities.java
@@ -1,15 +1,21 @@
 public class ArrayUtilities {
 
     public static int sum(int[] nums){
-        return 0;
+        int tot=0;
+        for (int j : nums) {
+            tot += j;
+        }
+
+        return tot;
     }
 
     public static int median(int[] nums){
-        return 0;
+        return nums[nums.length/2];
     }
 
     public static double mean(int[] nums){
-        return 0;
+        return (float)sum(nums)/nums.length;
     }
+
 
 }
