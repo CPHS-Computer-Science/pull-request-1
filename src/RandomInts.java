c16a79b63e04a9479914c7654f11ba4e0b92638f
AlexT <alex.thummas66@gmail.com>
2021-10-18 13:11:59 -0500

    Random Ints added

--- src/RandomInts.java
+++ src/RandomInts.java
@@ -4,6 +4,9 @@ public class RandomInts {
 
     public RandomInts(){
         numbers = new int[10];
+        for (int i = 0; i < numbers.length; i ++){
+            numbers[i] = (int) (Math.random() * 10);
+        }
     }
 
     public int[] getNumbers() {
