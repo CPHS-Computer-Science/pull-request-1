8400d8454de5e327c9e6dd225ab8ac7dd1b9a925
devinS711 <devin2014.ds@gmail.com>
2021-10-18 13:29:17 -0500

    Fixed so it now it returns random ints in the array.

--- src/RandomInts.java
+++ src/RandomInts.java
@@ -4,11 +4,14 @@ public class RandomInts {
 
     public RandomInts(){
         numbers = new int[10];
+        for (int i = 0; i < numbers.length; i++)
+                numbers[i] = (int)(Math.random()*10);
     }
 
     public int[] getNumbers() {
         return numbers;
     }
+
 
 
 }
